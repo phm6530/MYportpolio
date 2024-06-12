26961d093af358b73d906316c508857d838fee3e
hm <squirrel309@naver.com>
2024-06-12 18:39:09 +0900

    url 변경

--- vite-front/src/constants/apiUrl.ts
+++ vite-front/src/constants/apiUrl.ts
@@ -1,11 +1,10 @@
 // dev
 // export const ENDPOINT_URL = 'http://localhost:8080';
-export const ENDPOINT_URL =
-    'http://ec2-3-38-247-140.ap-northeast-2.compute.amazonaws.com/api/';
-
-export const IMG_UPLOAD_PATH = 'uploads';
 
 // prod S3 storate
-export const IMG_URL = `https://myportpolio-storage-s3.s3.ap-northeast-2.amazonaws.com`;
+export const ENDPOINT_URL =
+    'http://ec2-3-38-247-140.ap-northeast-2.compute.amazonaws.com/api/';
+export const IMG_URL = `https://d33h8icwcso2tj.cloudfront.net`;
 
+export const IMG_UPLOAD_PATH = 'uploads';
 // `${ENDPOINT_URL}/${IMG_UPLOAD_PATH}/`,
